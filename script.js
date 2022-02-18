476d61626515110faa45e38ce76b6116db692705
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-02-18 12:04:30 +0000

    add switch statement so amount changes with progress range slider

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const slider = document.querySelector("#slider");
 const output = document.querySelector(".price-amount");
 const progressBar = document.querySelector(".progress-bar");
-
-output.innerText = slider.value;
+const pageviews = document.querySelector(".pageviews");
 
 slider.oninput = function () {
   output.innerText = this.value;
   progressBar.style.width = this.value + "%";
+
+  switch (output.innerText) {
+    case "0":
+      output.innerText = "$8.00";
+      break;
+    case "25":
+      output.innerText = "$12.00";
+      break;
+    case "50":
+      output.innerText = "$16.00";
+      break;
+    case "75":
+      output.innerText = "$24.00";
+      break;
+    case "100":
+      output.innerText = "$36.00";
+      break;
+  }
 };
