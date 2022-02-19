28deb370ad96db2a18db4a933f056281b5041661
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-02-19 11:41:40 +0000

    change to £ symbol

--- script.js
+++ script.js
@@ -15,23 +15,23 @@ slider.oninput = function () {
   if (toggle.checked === false) {
     switch (output.innerText) {
       case "0":
-        output.innerText = "$8.00";
+        output.innerText = "£8.00";
         pageviews.innerText = "10K pageviews";
         break;
       case "25":
-        output.innerText = "$12.00";
+        output.innerText = "£12.00";
         pageviews.innerText = "50K pageviews";
         break;
       case "50":
-        output.innerText = "$16.00";
+        output.innerText = "£16.00";
         pageviews.innerText = "100K pageviews";
         break;
       case "75":
-        output.innerText = "$24.00";
+        output.innerText = "£24.00";
         pageviews.innerText = "500K pageviews";
         break;
       case "100":
-        output.innerText = "$36.00";
+        output.innerText = "£36.00";
         pageviews.innerText = "1M pageviews";
         break;
     }
@@ -40,23 +40,23 @@ slider.oninput = function () {
   if (toggle.checked === true) {
     switch (output.innerText) {
       case "0":
-        output.innerText = "$6.00";
+        output.innerText = "$£.00";
         pageviews.innerText = "10K pageviews";
         break;
       case "25":
-        output.innerText = "$9.00";
+        output.innerText = "$£.00";
         pageviews.innerText = "50K pageviews";
         break;
       case "50":
-        output.innerText = "$12.00";
+        output.innerText = "£12.00";
         pageviews.innerText = "100K pageviews";
         break;
       case "75":
-        output.innerText = "$18.00";
+        output.innerText = "£18.00";
         pageviews.innerText = "500K pageviews";
         break;
       case "100":
-        output.innerText = "$27.00";
+        output.innerText = "£27.00";
         pageviews.innerText = "1M pageviews";
         break;
     }
@@ -67,30 +67,30 @@ slider.oninput = function () {
 
 toggle.addEventListener("click", (e) => {
   if (toggle.checked === true) {
-    const replacedAmount = output.innerText.replace(/\$/g, "");
+    const replacedAmount = output.innerText.replace(/\£/g, "");
     const discount = parseFloat(replacedAmount) * 0.25;
     const discountTotal = replacedAmount - discount;
-    output.innerText = `$${discountTotal.toFixed(2)}`;
+    output.innerText = `£${discountTotal.toFixed(2)}`;
   }
 
   //   Event listener for when toggle button is clicked back to monthly
 
   if (toggle.checked === false) {
     switch (output.innerText) {
-      case "$6.00":
-        output.innerText = "$8.00";
+      case "£6.00":
+        output.innerText = "£8.00";
         break;
-      case "$9.00":
-        output.innerText = "$12.00";
+      case "£9.00":
+        output.innerText = "£12.00";
         break;
-      case "$12.00":
-        output.innerText = "$16.00";
+      case "£12.00":
+        output.innerText = "£16.00";
         break;
-      case "$18.00":
-        output.innerText = "$24.00";
+      case "£18.00":
+        output.innerText = "£24.00";
         break;
-      case "$27.00":
-        output.innerText = "$36.00";
+      case "£27.00":
+        output.innerText = "£36.00";
         break;
     }
   }
